0e63542bbee52dcc9439360a5c7d788471e083e4
Pyhajarvi Maaret <maaret.pyhajarvi@vaisala.com>
2023-02-22 20:02:35 +0200

    More tests

--- conftest.py
+++ conftest.py
@@ -1,11 +1,19 @@
 import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import Page, BrowserContext
 
 @pytest.fixture(scope="function")
-def page_to_url(page) -> Page:
+def page_to_url(page: Page) -> Page:
     page.goto('https://todomvc.com/examples/vanillajs/')
-    return page
+    yield page
 
-
+#marker example - can run tests with 'pytest -m "my_markers"'
 def pytest_configure(config):
     config.addinivalue_line("markers", "my_markers: this is how my marker is used")
+
+@pytest.fixture(scope="session")
+def my_browser(browser) -> Page:
+    c = browser.new_context()
+    p = c.new_page()
+    p.goto('https://todomvc.com/examples/vanillajs/')
+    yield p
+    c.close()
--- test_basic.py
+++ test_basic.py
@@ -1,12 +1,17 @@
 import pytest
-from playwright.sync_api import expect
+from playwright.sync_api import expect, BrowserContext
 
-@pytest.mark.my_markers("this")
+
 @pytest.mark.parametrize("input_text", ["test", "foo bar baz "], ids=["first", "foobarbaz"])
 def test_add_todo(page_to_url, input_text):
     page_to_url.locator(".new-todo").fill(input_text)
     page_to_url.locator(".new-todo").press("Enter")
     expect(page_to_url.locator(".todo-list li label")).to_have_text(input_text.strip())
+
+def test_add_todo_with_off_focus(page_to_url):
+    page_to_url.locator(".new-todo").fill("test")
+    page_to_url.locator("footer.info").click()
+    expect(page_to_url.locator(".todo-list li label")).to_have_text("test")
 
 def test_should_not_add_empty_todos(page_to_url):
     page_to_url.locator(".new-todo").fill("")
@@ -24,3 +29,18 @@ def test_todo_deleted(page_to_url):
     page_to_url.locator(".new-todo").press("Enter")
     page_to_url.locator(".todo-list").hover()
     page_to_url.locator(".destroy").click()
+
+def test_counter_displays_nb_of_todos(page_to_url):
+#should display the current number of todo items
+    page_to_url.locator(".new-todo").fill("first todo")
+    page_to_url.keyboard.press("Enter")
+    page_to_url.locator(".new-todo").fill("second todo")
+    page_to_url.keyboard.press("Enter")
+    expect(page_to_url.locator(".todo-count")).to_have_text("2 items left")
+
+@pytest.mark.my_markers("this")
+def test_data_persists_on_local_storage(my_browser):
+    my_browser.locator(".new-todo").fill("first todo")
+    my_browser.keyboard.press("Enter") 
+    my_browser.context.new_page().goto('https://todomvc.com/examples/vanillajs/')
+    expect(my_browser.locator(".todo-count")).to_have_text("1 item left")
